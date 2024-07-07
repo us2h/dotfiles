6e4036a13c028a549aadd33f0525beaba241c298
Konstantin Dedishchev <us2h@mm.st>
2024-07-07 18:11:38 +0200

    brew install fix, ohmyzsh install, few apps added

--- macos/setup.zsh
+++ macos/setup.zsh
@@ -1,7 +1,12 @@
 #!/bin/zsh
 
+# Install OhMyZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/$(whoami)/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Apps and Dependencies
 brew bundle
