eaac4ea46a10dbcf4a2815b1c16ffa578cb1c3fe
Konstantin Dedishchev <us2h@mm.st>
2026-05-09 21:45:04 +0200

    dotfiles for KDE

---
+++ kde6/save-kde-configs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# save-kde.sh
+
+DOTFILES=~/dotfiles
+KDE=$DOTFILES/kde6/.config
+KONSOLE_CONF=$DOTFILES/konsole/.config
+KONSOLE_DATA=$DOTFILES/konsole/.local/share/konsole
+
+mkdir -p $KDE $KONSOLE_CONF $KONSOLE_DATA
+
+KDE_FILES=(
+  kdeglobals
+  kwinrc
+  plasmashellrc
+  plasma-org.kde.plasma.desktop-appletsrc
+  kglobalshortcutsrc
+  khotkeysrc
+  kcminputrc
+  kxkbrc
+)
+
+for f in "${KDE_FILES[@]}"; do
+  [[ -f ~/.config/$f ]] && cp ~/.config/$f $KDE/
+done
+
+# Konsole
+[[ -f ~/.config/konsolerc ]] && cp ~/.config/konsolerc $KONSOLE_CONF/
+cp ~/.local/share/konsole/*.profile $KONSOLE_DATA/ 2>/dev/null
+
+echo "Done"
---
+++ konsole/.local/share/konsole/us2h.profile
@@ -0,0 +1,6 @@
+[Appearance]
+Font=Hack,18,-1,5,700,0,0,0,0,0,0,0,0,0,0,1,Bold,0,0
+
+[General]
+Name=us2h
+Parent=FALLBACK/
